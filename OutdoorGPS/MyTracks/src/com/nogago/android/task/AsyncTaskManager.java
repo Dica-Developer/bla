67c66b646b6ae057e4faaf5591162ba6e4da1fe2
volz <volz@09f8092a-7451-4aaf-b957-db2732368af2>
2013-10-13 16:49:26 +0000

    Test #644

--- OutdoorGPS/MyTracks/src/com/nogago/android/task/AsyncTaskManager.java
+++ OutdoorGPS/MyTracks/src/com/nogago/android/task/AsyncTaskManager.java
@@ -9,115 +9,115 @@ import android.content.DialogInterface.OnCancelListener;
  * Utility Manager handling ProgressDialog display for arbitrary long running
  * asynchronous tasks that can be set via setupTask and retained via retainTask
  */
-public final class AsyncTaskManager implements IProgressTracker,
-        OnCancelListener {
+public final class AsyncTaskManager implements IProgressTracker, OnCancelListener {
 
-    private final OnTaskCompleteListener mTaskCompleteListener;
-    private final ProgressDialog mProgressDialog;
-    private TrackableTask mAsyncTask;
-    private String message;
+  private final OnTaskCompleteListener mTaskCompleteListener;
+  private final ProgressDialog mProgressDialog;
+  private TrackableTask mAsyncTask;
+  private String message;
 
-    /**
-     * Constructor requires context of calling Activity for UI work and provides
-     * callback to OnTaskCompleteListener and a message displayed to the user
-     * while waiting for Task to complete
-     */
-    public AsyncTaskManager(Context context,
-            OnTaskCompleteListener taskCompleteListener, String msg,
-            boolean countable) {
-        this.message = msg;
-        // Save reference to complete listener (activity)
-        mTaskCompleteListener = taskCompleteListener;
-        // Setup progress dialog
-        mProgressDialog = new ProgressDialog(context);
-        if (message != null)
-            mProgressDialog.setMessage(message);
-        if (countable)
-            mProgressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
-        else
-            mProgressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-        mProgressDialog.setProgress(0);
-        mProgressDialog.setCancelable(true);
-        mProgressDialog.setOnCancelListener(this);
+  /**
+   * Constructor requires context of calling Activity for UI work and provides
+   * callback to OnTaskCompleteListener and a message displayed to the user
+   * while waiting for Task to complete
+   */
+  public AsyncTaskManager(Context context, OnTaskCompleteListener taskCompleteListener, String msg,
+      boolean countable) {
+    this.message = msg;
+    // Save reference to complete listener (activity)
+    mTaskCompleteListener = taskCompleteListener;
+    // Setup progress dialog
+    mProgressDialog = new ProgressDialog(context);
+    if (message != null)
+      mProgressDialog.setMessage(message);
+    if (countable)
+      mProgressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+    else
+      mProgressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+    mProgressDialog.setProgress(0);
+    mProgressDialog.setCancelable(true);
+    mProgressDialog.setOnCancelListener(this);
+  }
+
+  /** sets up the task to run and watch progress for */
+  public void setupTask(TrackableTask asyncTask) {
+    // Keep task
+    mAsyncTask = asyncTask;
+    // Wire task to tracker (this)
+    mAsyncTask.setProgressTracker(this);
+    // Start task
+    mAsyncTask.execute();
+  }
+
+  @Override
+  public void setProgressMessage(String msg) {
+    this.message = msg;
+  }
+
+  /** Call back from task to update the progress */
+  @Override
+  public void onProgress(int percentage) {
+    // Show dialog if it wasn't shown yet or was removed on configuration
+    // (rotation) change
+    if (!mProgressDialog.isShowing()) {
+      mProgressDialog.show();
     }
+    // Show current message in progress dialog
+    mProgressDialog.setProgress(percentage);
+    mProgressDialog.setMessage(message);
+  }
 
-    /** sets up the task to run and watch progress for */
-    public void setupTask(TrackableTask asyncTask) {
-        // Keep task
-        mAsyncTask = asyncTask;
-        // Wire task to tracker (this)
-        mAsyncTask.setProgressTracker(this);
-        // Start task
-        mAsyncTask.execute();
+  /** Call back to notify of cancellation */
+  @Override
+  public void onCancel(DialogInterface dialog) {
+    // Cancel task
+    if (mProgressDialog.isShowing())
+      mProgressDialog.dismiss();
+    if (mAsyncTask == null) {
+      return;
     }
+    mAsyncTask.cancel(true);
+    // Notify activity about completion
+    mTaskCompleteListener.onTaskCancel(mAsyncTask);
+    // Reset task
+    mAsyncTask = null;
+  }
 
-    @Override
-    public void setProgressMessage(String msg) {
-        this.message = msg;
-    }
+  /** Call back to notify of completion */
+  @Override
+  public void onComplete(Object result) {
+    // Close progress dialog
+    if (mProgressDialog != null && mProgressDialog.isShowing())
+      mProgressDialog.dismiss();
+    // Notify activity about completion
+    if (result instanceof Exception)
+      mTaskCompleteListener.onTaskError(mAsyncTask, (Exception) result);
+    else
+      mTaskCompleteListener.onTaskComplete(mAsyncTask, result);
 
-    /** Call back from task to update the progress */
-    @Override
-    public void onProgress(int percentage) {
-        // Show dialog if it wasn't shown yet or was removed on configuration
-        // (rotation) change
-        if (!mProgressDialog.isShowing()) {
-            mProgressDialog.show();
-        }
-        // Show current message in progress dialog
-        mProgressDialog.setProgress(percentage);
-        mProgressDialog.setMessage(message);
-    }
+    // Reset task
+    mAsyncTask = null;
+  }
 
-    /** Call back to notify of cancellation */
-    @Override
-    public void onCancel(DialogInterface dialog) {
-        // Cancel task
-        if(mProgressDialog.isShowing()) mProgressDialog.dismiss();
-        if(mAsyncTask == null) {
-            return;
-        }
-        mAsyncTask.cancel(true);
-        // Notify activity about completion
-        mTaskCompleteListener.onTaskCancel(mAsyncTask);
-        // Reset task
-        mAsyncTask = null;
+  public Object retainTask() {
+    // Detach task from tracker (this) before retain
+    if (mAsyncTask != null) {
+      mAsyncTask.setProgressTracker(null);
     }
+    // Retain task
+    return mAsyncTask;
+  }
 
-    /** Call back to notify of completion */
-    @Override
-    public void onComplete(Object result) {
-        // Close progress dialog
-        mProgressDialog.dismiss();
-        // Notify activity about completion
-        if(result instanceof Exception) 
-        mTaskCompleteListener.onTaskError(mAsyncTask, (Exception) result);
-        else
-          mTaskCompleteListener.onTaskComplete(mAsyncTask, result);
-        
-          // Reset task
-        mAsyncTask = null;
+  public void handleRetainedTask(Object instance) {
+    // Restore retained task and attach it to tracker (this)
+    if (instance instanceof TrackableTask) {
+      mAsyncTask = (TrackableTask) instance;
+      mAsyncTask.setProgressTracker(this);
     }
+  }
 
-    public Object retainTask() {
-        // Detach task from tracker (this) before retain
-        if (mAsyncTask != null) {
-            mAsyncTask.setProgressTracker(null);
-        }
-        // Retain task
-        return mAsyncTask;
-    }
-
-    public void handleRetainedTask(Object instance) {
-        // Restore retained task and attach it to tracker (this)
-        if (instance instanceof TrackableTask) {
-            mAsyncTask = (TrackableTask) instance;
-            mAsyncTask.setProgressTracker(this);
-        }
-    }
-
-    public boolean isWorking() {
-        // Track current status
-        return mAsyncTask != null;
-    }
+  public boolean isWorking() {
+    // Track current status
+    return mAsyncTask != null;
+  }
 }
