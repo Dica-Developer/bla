03502afe2560b17e0ea3508ef9332fb9633da167
volz <volz@09f8092a-7451-4aaf-b957-db2732368af2>
2013-11-08 15:26:31 +0000

    Fixed #733

--- Maps/nogagoMaps/src/com/nogago/android/maps/activities/MainMenuActivity.java
+++ Maps/nogagoMaps/src/com/nogago/android/maps/activities/MainMenuActivity.java
@@ -80,21 +80,21 @@ public class MainMenuActivity extends FragmentActivity {
 									new String[] { Constants.BUGS_MAIL }); //$NON-NLS-1$
 							intent.setType("vnd.android.cursor.dir/email"); //$NON-NLS-1$
 							intent.putExtra(Intent.EXTRA_SUBJECT,
-									"nogago Tracks bug"); //$NON-NLS-1$
+									"nogago Maps bug"); //$NON-NLS-1$
 							StringBuilder text = new StringBuilder();
 							text.append("\nDevice : ").append(Build.DEVICE); //$NON-NLS-1$
 							text.append("\nBrand : ").append(Build.BRAND); //$NON-NLS-1$
 							text.append("\nModel : ").append(Build.MODEL); //$NON-NLS-1$
-							text.append("\nProduct : ").append("Tracks"); //$NON-NLS-1$
+							text.append("\nProduct : ").append("Maps"); //$NON-NLS-1$
 							text.append("\nBuild : ").append(Build.DISPLAY); //$NON-NLS-1$
 							text.append("\nVersion : ").append(Build.VERSION.RELEASE); //$NON-NLS-1$
-							text.append("\nApp Starts : ").append(com.nogago.android.maps.utils.EulaUtils.getAppStart(MainMenuActivity.this)); //$NON-NLS-1$
+							text.append("\nApp Starts : ").append(com.nogago.android.maps.utils.EulaUtils.getAppStart(MainMenuActivity.this)).append("\n"); //$NON-NLS-1$
 
 							try {
 								PackageInfo info = getPackageManager()
 										.getPackageInfo(getPackageName(), 0);
 								if (info != null) {
-									text.append("\nApk Version : ").append(info.versionName).append(" ").append(info.versionCode); //$NON-NLS-1$ //$NON-NLS-2$
+									text.append("\nApk Version : ").append(info.versionName).append(" ").append(info.versionCode).append("\n"); //$NON-NLS-1$ //$NON-NLS-2$
 								}
 							} catch (NameNotFoundException e) {
 							}
@@ -102,12 +102,11 @@ public class MainMenuActivity extends FragmentActivity {
 							try {
 								FileReader fr = new FileReader(file);
 								BufferedReader br = new BufferedReader(fr);
-								String line;
-								while (br.read() != -1) {
-									if ((line = br.readLine()) != null) {
-										text.append(line);
-									}
-								}
+								String line = null;
+								do {
+									line = br.readLine();
+									if(line != null) text.append(line).append("\n");
+								} while (line != null);
 								br.close();
 								fr.close();
 							} catch (IOException e) {
